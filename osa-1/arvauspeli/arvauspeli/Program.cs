2d5d911f228a91cf932784cb5869f0962cb66922
s2101048 <2101048@edu.bc.fi>
2023-02-07 17:01:01 +0200

    lisätty ohjelmat

---
+++ osa-1/arvauspeli/arvauspeli/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    public static int NroLukija(string prompt)
+    {
+        int arvaus = 0;
+        while (true)
+        {
+            Console.Write(prompt);
+            string arvausTeksi = Console.ReadLine();
+            try
+            {
+                arvaus = Convert.ToInt32(arvausTeksi);
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("et antanut numeroa");
+                continue;
+            }
+        }
+        return arvaus;
+    }
+
+    public static void Main(string[] args)
+    {
+        Random rng = new Random();
+        int min = 1; int max = 20;
+        foreach (string s in args)
+        {
+            string parametri;
+
+
+        }
+    }
+}
---
+++ osa-1/dvd/dvd/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int width = Console.WindowWidth;
+        int height = Console.WindowHeight;
+
+        int center_x = width / 2;
+        int center_y = height / 2;
+
+        int speed_x = 1;
+        int speed_y = 1;
+        int text_x = center_x;
+        int text_y = center_y;
+
+        Console.BackgroundColor = ConsoleColor.DarkRed;
+        Console.ForegroundColor = ConsoleColor.White;
+
+        while(true)
+        {
+            Thread.Sleep(33);
+            text_x += speed_x;
+            text_y += speed_y;
+
+            int text_width = 3;
+            if (text_x >= width - text_width)
+            {
+                text_x = width - text_width;
+                speed_x *= -1;
+            }
+            if (text_x <= 0)
+            {
+                text_x = 0;
+                speed_x *= -1;
+            }
+            if (text_y >= height)
+            {
+                text_y = height;
+                speed_y *= -1;
+            }
+            if (text_y <= 1)
+            {
+                text_y = 1;
+                speed_y *= -1;
+            }
+            Console.Clear();
+            Console.SetCursorPosition(text_x, text_y);
+            Console.Write("DVD");
+        }
+    }
+}
